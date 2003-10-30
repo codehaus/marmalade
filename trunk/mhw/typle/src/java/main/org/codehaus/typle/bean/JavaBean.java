154b77d8eccd706a489969098ea4384a18250e2e
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-10-30 14:44:45 +0000

    Early check-in of JavaBean generator.

---
+++ trunk/mhw/typle/src/java/main/org/codehaus/typle/bean/JavaBean.java
@@ -0,0 +1,112 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.typle.bean;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.codehaus.typle.JavaPrimitive;
+import org.codehaus.typle.JavaReferenceType;
+import org.codehaus.typle.NamedType;
+import org.codehaus.typle.NamedTypeList;
+import org.codehaus.typle.RecordType;
+import org.codehaus.typle.Type;
+import org.codehaus.typle.src.BoilerPlateComment;
+import org.codehaus.typle.src.SourceFile;
+import org.codehaus.typle.src.java.Field;
+import org.codehaus.typle.src.java.Import;
+import org.codehaus.typle.src.java.JavaClass;
+import org.codehaus.typle.src.java.JavaSource;
+import org.codehaus.typle.src.java.Package;
+
+/**
+ * @author Mark H. Wilkinson
+ * @version $Revision$
+ */
+public final class JavaBean {
+    private final String beanName;
+
+    private final RecordType type;
+
+    private final JavaBeanType javaBeanType;
+
+    public JavaBean(String beanName, RecordType type, JavaBeanType javaBeanType)
+    {
+        this.beanName = beanName;
+        this.type = type;
+        this.javaBeanType = javaBeanType;
+    }
+
+    /**
+     * @return
+     */
+    public RecordType getType() {
+        return type;
+    }
+
+    /**
+     * @return
+     */
+    public JavaBeanType getJavaBeanType() {
+        return javaBeanType;
+    }
+
+    /**
+     * @param stream
+     */
+    public void generate(PrintWriter writer) throws IOException {
+        SourceFile source = new SourceFile(writer, JavaSource.COMPARATOR);
+        State state = new State(source);
+        visit(type, state);
+        source.writeOutput();
+    }
+
+    private static class State {
+        SourceFile source;
+        JavaClass outputClass;
+
+        public State(SourceFile source) {
+            this.source = source;
+        }
+    }
+
+    /**
+     * @param record
+     */
+    private void visit(RecordType record, State state) {
+        state.source.add(new BoilerPlateComment("/*\n * $" + "Id$\n */\n\n", 1));
+        state.source.add(new Package("net.kremvax"));
+        state.source.add(new Package("foo.kremvax"));
+        state.source.add(new Import("org.codehaus.plexus.Plexus"));
+        state.source.add(new Import("javax.servlet.Servlet"));
+
+        state.outputClass = new JavaClass(beanName);
+        state.source.add(state.outputClass);
+        NamedTypeList fields = record.getFields();
+        for (int i = 0; i < fields.size(); i++) {
+            visit(fields.get(i), state);
+        }
+        state.outputClass = null;
+    }
+
+    /**
+     * @param type
+     */
+    private void visit(NamedType type, State state) {
+        Type t = type.getType();
+        if (t instanceof JavaReferenceType) {
+            JavaReferenceType javaType = (JavaReferenceType) t;
+
+            if (!t.getName().startsWith("java.lang.")) {
+                state.source.add(new Import(t.getName()));
+            }
+            state.outputClass.add(new Field(javaType.getUnqualifiedName(),
+                                            type.getName()));
+        } else if (t instanceof JavaPrimitive) {
+            state.outputClass.add(new Field(type.getType().getName(),
+                                            type.getName()));
+        }
+    }
+}
--- trunk/mhw/typle/src/java/main/org/codehaus/typle/tools/BeanBuilder.java
+++ trunk/mhw/typle/src/java/main/org/codehaus/typle/tools/BeanBuilder.java
@@ -4,10 +4,30 @@
 
 package org.codehaus.typle.tools;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.codehaus.typle.Java;
+import org.codehaus.typle.JavaReferenceType;
+import org.codehaus.typle.NamedType;
+import org.codehaus.typle.RecordType;
+import org.codehaus.typle.bean.JavaBeanType;
+import org.codehaus.typle.bean.JavaBean;
+
 /**
  * @author Mark H. Wilkinson
  * @version $Revision$
  */
 public final class BeanBuilder {
-
+    public void run() throws IOException {
+        RecordType record = new RecordType(new NamedType[] {
+            new NamedType("intField", Java.INT_TYPE),
+            new NamedType("stringField",
+                          new JavaReferenceType("java.lang.String")),
+            new NamedType("fooField",
+                          new JavaReferenceType("org.codehaus.Foo")),
+        });
+        JavaBean bean = new JavaBean("Record", record, JavaBeanType.CLASS);
+        bean.generate(new PrintWriter(System.out, true));
+    }
 }
