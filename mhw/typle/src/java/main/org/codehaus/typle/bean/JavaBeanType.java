4d051aabfa9103a73a29f7f8ef2a2b3c216541b6
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-10-30 14:44:45 +0000

    Early check-in of JavaBean generator.

---
+++ mhw/typle/src/java/main/org/codehaus/typle/bean/JavaBeanType.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.typle.bean;
+
+/**
+ * Typesafe-enumeration representing the different kinds of JavaBean that
+ * can be generated.
+ *
+ * @author Mark H. Wilkinson
+ * @version $Revision$
+ */
+public final class JavaBeanType {
+    private final String name;
+
+    private JavaBeanType(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public static final JavaBeanType INTERFACE = new JavaBeanType("interface");
+
+    public static final JavaBeanType CLASS = new JavaBeanType("class");
+
+    public static final JavaBeanType ABSTRACT_CLASS = new JavaBeanType("abstract class");
+}
