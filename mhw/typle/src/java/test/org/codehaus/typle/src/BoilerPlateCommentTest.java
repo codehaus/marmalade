199130229af505536394d21d1d03160b989244b8
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-10-31 10:38:57 +0000

    Create test for AbstractSourceArtefact and BoilerPlateComment.

---
+++ mhw/typle/src/java/test/org/codehaus/typle/src/BoilerPlateCommentTest.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.typle.src;
+
+import java.io.PrintWriter;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link BoilerPlateComment} and {@link AbstractSourceArtefact}.
+ *
+ * @author Mark H. Wilkinson
+ * @version $Revision$
+ */
+public final class BoilerPlateCommentTest extends TestCase {
+    private BoilerPlateComment comment;
+
+    private SourceFile src;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        comment = new BoilerPlateComment("/* foo */", 5);
+        src = new SourceFile(new PrintWriter(System.out),
+                             SimpleSourceOrdering.COMPARATOR);
+    }
+
+    /**
+     * The basic accessors should return properties of the object.
+     */
+    public void testAccessors() {
+        assertEquals("/* foo */", comment.getCommentSource());
+        assertEquals(5, comment.getPriority());
+    }
+
+    /**
+     * Adding an artefact to a container should set the container field.
+     */
+    public void testContainment() {
+        assertNull(comment.getContainer());
+        src.add(comment);
+        assertEquals(src, comment.getContainer());
+    }
+
+    /**
+     * Make sure that adding an source artefact to a container more than
+     * once fails.
+     */
+    public void testMultipleAddition() {
+        src.add(comment);
+        try {
+            src.add(comment);
+            fail();
+        } catch (IllegalStateException e) {
+            // ignore
+        }
+    }
+}
