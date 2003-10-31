b435acdff7f11043bcb348cfd6ba2a7d304e4856
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-10-31 10:38:19 +0000

    Add some tests to hit nooks and crannies in the source.

--- trunk/mhw/typle/src/java/test/org/codehaus/typle/src/java/JavaSourceTest.java
+++ trunk/mhw/typle/src/java/test/org/codehaus/typle/src/java/JavaSourceTest.java
@@ -9,9 +9,12 @@ import java.util.Iterator;
 
 import junit.framework.TestCase;
 
+import org.codehaus.typle.src.BoilerPlateComment;
 import org.codehaus.typle.src.SourceFile;
 
 /**
+ * Tests for the Java source representation package.
+ *
  * @author Mark H. Wilkinson
  * @version $Revision$
  */
@@ -22,6 +25,40 @@ public class JavaSourceTest extends TestCase {
         super.setUp();
         src = new SourceFile(new PrintWriter(System.out),
                              JavaSource.COMPARATOR);
+    }
+
+    /**
+     * Check that boilerplate comments work.
+     */
+    public void testBoilerPlateComment() {
+        src.add(new BoilerPlateComment("/* Header */", 2));
+        src.add(new BoilerPlateComment("/* Date */", 10));
+        src.add(new BoilerPlateComment("/* Id */", 5));
+        src.add(new BoilerPlateComment("/* Do not edit */", 1));
+
+        Iterator iter = src.iterator();
+        BoilerPlateComment c;
+        assertTrue(iter.hasNext());
+        c = (BoilerPlateComment) iter.next();
+        assertEquals("/* Do not edit */", c.getCommentSource());
+        assertEquals(1, c.getPriority());
+
+        assertTrue(iter.hasNext());
+        c = (BoilerPlateComment) iter.next();
+        assertEquals("/* Header */", c.getCommentSource());
+        assertEquals(2, c.getPriority());
+
+        assertTrue(iter.hasNext());
+        c = (BoilerPlateComment) iter.next();
+        assertEquals("/* Id */", c.getCommentSource());
+        assertEquals(5, c.getPriority());
+
+        assertTrue(iter.hasNext());
+        c = (BoilerPlateComment) iter.next();
+        assertEquals("/* Date */", c.getCommentSource());
+        assertEquals(10, c.getPriority());
+
+        assertFalse(iter.hasNext());
     }
 
     /**
@@ -108,6 +145,41 @@ public class JavaSourceTest extends TestCase {
     }
 
     /**
+     * Test ordering of top-level java constructs.
+     */
+    public void testJavaConstructOrder() {
+        src.add(new JavaClass(Modifier.PRIVATE, "Abc"));
+        src.add(new JavaClass("Xyz"));
+        src.add(new Import("org.codehaus.Foo"));
+        src.add(new Package("org.codehaus.typle"));
+        src.add(new BoilerPlateComment("/* Header */", 1));
+
+        Iterator iter = src.iterator();
+        Object o;
+        JavaClass c;
+        assertTrue(iter.hasNext());
+        o = iter.next();
+        assertEquals(BoilerPlateComment.class, o.getClass());
+        assertTrue(iter.hasNext());
+        o = iter.next();
+        assertEquals(Package.class, o.getClass());
+        assertTrue(iter.hasNext());
+        o = iter.next();
+        assertEquals(Import.class, o.getClass());
+        assertTrue(iter.hasNext());
+        o = iter.next();
+        assertEquals(JavaClass.class, o.getClass());
+        c = (JavaClass) o;
+        assertEquals("Xyz", c.getClassName());
+        assertTrue(iter.hasNext());
+        o = iter.next();
+        assertEquals(JavaClass.class, o.getClass());
+        c = (JavaClass) o;
+        assertEquals("Abc", c.getClassName());
+        assertFalse(iter.hasNext());
+    }
+
+    /**
      * Test ordering of public java classes, despite the fact that the
      * resulting source file would be invalid.
      */
@@ -134,7 +206,7 @@ public class JavaSourceTest extends TestCase {
      * Verify that Java classes are ordered correctly: public first, then
      * others in alphabetical order.
      */
-    public void testJavaClassOrdering() {
+    public void testJavaClassOrdering1() {
         src.add(new JavaClass("Def"));
         src.add(new JavaClass(Modifier.PRIVATE, "Abc"));
         src.add(new JavaClass(Modifier.PRIVATE, "Xyz"));
@@ -154,6 +226,26 @@ public class JavaSourceTest extends TestCase {
         assertTrue(iter.hasNext());
         c = (JavaClass) iter.next();
         assertEquals("Xyz", c.getClassName());
+        assertFalse(iter.hasNext());
+    }
+
+    /**
+     * Verify that Java classes are ordered correctly: public first, then
+     * others in alphabetical order. Additional test to hit a different
+     * branch in javaClassCompare.
+     */
+    public void testJavaClassOrdering2() {
+        src.add(new JavaClass(Modifier.PRIVATE, "Abc"));
+        src.add(new JavaClass("Def"));
+
+        Iterator iter = src.iterator();
+        JavaClass c;
+        assertTrue(iter.hasNext());
+        c = (JavaClass) iter.next();
+        assertEquals("Def", c.getClassName());
+        assertTrue(iter.hasNext());
+        c = (JavaClass) iter.next();
+        assertEquals("Abc", c.getClassName());
         assertFalse(iter.hasNext());
     }
 }
