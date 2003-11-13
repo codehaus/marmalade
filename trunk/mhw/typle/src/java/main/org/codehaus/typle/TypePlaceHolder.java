1ebaa51e2ec1712dc53ce87cb12a194e5b647ebf
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-11-13 16:21:01 +0000

    Add TypePlaceHolder to allow indirection within the type graph.

---
+++ trunk/mhw/typle/src/java/main/org/codehaus/typle/TypePlaceHolder.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.typle;
+
+/**
+ * A <code>TypePlaceHolder</code> acts as a stand-in for a real data type
+ * object at times when the real data type might not exist already. For
+ * example, when reading multiple type definitions from a data source some
+ * data types may reference other data types from the same source that have
+ * not yet been created.
+ *
+ * @author Mark Wilkinson
+ * @version $Revision$
+ */
+public final class TypePlaceHolder implements Type {
+    private final String name;
+    private transient Type type;
+
+    /**
+     * Constructor for <code>TypePlaceHolder</code>s.
+     *
+     * @param name Type's name.
+     */
+    public TypePlaceHolder(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @see Type#getTypeName()
+     */
+    public String getTypeName() {
+        if (type == null)
+            return name;
+        return type.getTypeName();
+    }
+
+    public Type getWrappedType() {
+        return type;
+    }
+
+    public void resolve() throws TypeLookupException {
+        type = Naming.lookup(name);
+    }
+}
