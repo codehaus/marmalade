2bc6dfb4ae44b6438c66eb6c37fdc47f758a691a
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-11-17 18:27:39 +0000

    Add a Method object.

---
+++ mhw/typle/src/java/main/org/codehaus/typle/src/java/Method.java
@@ -0,0 +1,72 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.typle.src.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.codehaus.typle.BindingList;
+import org.codehaus.typle.Function;
+import org.codehaus.typle.src.AbstractSourceArtefact;
+import org.codehaus.typle.src.SourceArtefact;
+
+/**
+ * @author Mark H. Wilkinson
+ * @version $Revision$
+ */
+public final class Method
+    extends AbstractSourceArtefact
+    implements SourceArtefact
+{
+    private final Modifier[] modifiers;
+
+    private final String name;
+
+    private final Function signature;
+
+    private final String body;
+
+    private static final Modifier[] DEFAULT_MODIFIERS = new Modifier[] {
+        Modifier.PUBLIC,
+    };
+
+    public Method(String name, Function signature, String body) {
+        this(DEFAULT_MODIFIERS, name, signature, body);
+    }
+
+    public Method(Modifier[] modifiers, String name, Function signature,
+        String body)
+    {
+        this.modifiers = modifiers;
+        this.name = name;
+        this.signature = signature;
+        this.body = body;
+    }
+
+    public String getMethodName() {
+        return name;
+    }
+
+    public void write(PrintWriter writer) throws IOException {
+        String mods = Modifier.toString(modifiers);
+        String space = (mods.length() == 0)? "" : " ";
+        writer.print(mods + space);
+        writer.println(formatSignature(name, signature) + " {");
+        writer.println(body);
+        writer.println("}");
+    }
+
+    private String formatSignature(String name, Function signature) {
+        BindingList parameters = signature.getFormalParameters();
+        StringBuffer buf = new StringBuffer();
+        buf.append(signature.getReturnType().getTypeName());
+        buf.append(' ');
+        buf.append(name);
+        buf.append('(');
+        buf.append(signature.getFormalParameters().signature());
+        buf.append(')');
+        return buf.toString();
+    }
+}
