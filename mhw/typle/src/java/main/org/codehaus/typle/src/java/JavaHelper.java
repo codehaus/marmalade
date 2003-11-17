c73bc6cc594847fe6abc734f28aa25a9d423f33a
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-11-17 14:49:31 +0000

    Bit of refactoring - pull simple Java artefact creation into JavaHelper and reorganise JavaBean a bit.

---
+++ mhw/typle/src/java/main/org/codehaus/typle/src/java/JavaHelper.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.typle.src.java;
+
+import org.codehaus.typle.JavaNames;
+import org.codehaus.typle.src.BoilerPlateComment;
+import org.codehaus.typle.src.SourceContainer;
+
+/**
+ * Simple helper methods for creating Java artefacts.
+ *
+ * @author Mark H. Wilkinson
+ * @version $Revision$
+ */
+public final class JavaHelper {
+    // prevent instantiation
+    private JavaHelper() {
+    }
+
+    private static final String BOILERPLATE_COMMENT
+        = "/*\n"
+        + " * Automatically generated. Do not edit.\n"
+        + " */\n\n";
+
+    /**
+     * Create the basic contents of a Java source file. This comprises
+     * a boilerplate 'do not edit' comment, the package declaration and
+     * the class declaration.
+     *
+     * @param source The source container to add the content to.
+     * @param className The name of the class that is being generated.
+     * @return The <code>JavaClass</code> that represents the public
+     * class added to the <code>SourceContainer</code>.
+     */
+    public static JavaClass initSourceFile(SourceContainer source,
+        String className)
+    {
+        source.add(new BoilerPlateComment(BOILERPLATE_COMMENT, 1));
+        source.add(new JavaPackage(JavaNames.packageName(className)));
+        JavaClass srcClass = new JavaClass(JavaNames.className(className));
+        source.add(srcClass);
+        return srcClass;
+    }
+}
