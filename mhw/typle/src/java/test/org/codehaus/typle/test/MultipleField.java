295c6b6b3cb811beee93310ae1d932d7929453fd
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2003-12-01 17:45:51 +0000

    Add expected output from MultipleField test.

---
+++ mhw/typle/src/java/test/org/codehaus/typle/test/MultipleField.java
@@ -0,0 +1,38 @@
+/*
+ * Automatically generated. Do not edit.
+ */
+
+package org.codehaus.typle.test;
+
+
+public final class MultipleField {
+private String fieldOne;
+
+private float fieldThree;
+
+private int fieldTwo;
+
+public java.lang.String getFieldOne() {
+return fieldOne;
+}
+
+public float getFieldThree() {
+return fieldThree;
+}
+
+public int getFieldTwo() {
+return fieldTwo;
+}
+
+public void setFieldOne(java.lang.String fieldOne) {
+this.fieldOne = fieldOne;
+}
+
+public void setFieldThree(float fieldThree) {
+this.fieldThree = fieldThree;
+}
+
+public void setFieldTwo(int fieldTwo) {
+this.fieldTwo = fieldTwo;
+}
+}
