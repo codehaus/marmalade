60324981435d588a828497b251cc9f026bbf3267
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-10 23:52:01 +0000

    Flesh out the unit tests a bit more, to the point where they should run (if I haven't missed something).

--- trunk/mhw/tagalog/src/test/org/codehaus/tagalog/acceptance/people/PeopleTagLibrary.java
+++ trunk/mhw/tagalog/src/test/org/codehaus/tagalog/acceptance/people/PeopleTagLibrary.java
@@ -16,6 +16,7 @@ public final class PeopleTagLibrary extends AbstractTagLibrary {
     public static final String NS_URI = "tagalog:people";
 
     public PeopleTagLibrary() {
+        registerTag("people", PeopleTag.class);
         registerTag("person", PersonTag.class);
         registerTag("first-name", AttributeTag.class);
         registerTag("last-name", AttributeTag.class);
