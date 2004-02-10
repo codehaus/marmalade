ce077ba3321ed5b478582a2aa17c359167249750
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-10 23:52:01 +0000

    Flesh out the unit tests a bit more, to the point where they should run (if I haven't missed something).

---
+++ mhw/tagalog/src/test/org/codehaus/tagalog/acceptance/people/PeopleTag.java
@@ -0,0 +1,17 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.tagalog.acceptance.people;
+
+import org.codehaus.tagalog.Tag;
+
+/**
+ * PeopleTag
+ *
+ * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
+ * @version $Revision$
+ */
+public class PeopleTag implements Tag {
+
+}
