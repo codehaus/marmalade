9199e728a3cac2136c0abfa64a297a8cc5be682d
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-11 12:45:34 +0000

    Flesh out the implementation of the tags, now we've worked out what the Tag lifecycle might look like.

--- mhw/tagalog/src/test/org/codehaus/tagalog/acceptance/people/AttributeTag.java
+++ mhw/tagalog/src/test/org/codehaus/tagalog/acceptance/people/AttributeTag.java
@@ -4,7 +4,8 @@
 
 package org.codehaus.tagalog.acceptance.people;
 
-import org.codehaus.tagalog.Tag;
+import org.codehaus.tagalog.AbstractTag;
+import org.codehaus.tagalog.TagalogParseException;
 
 /**
  * AttributeTag
@@ -12,6 +13,23 @@ import org.codehaus.tagalog.Tag;
  * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
  * @version $Revision$
  */
-public final class AttributeTag implements Tag {
+public final class AttributeTag extends AbstractTag {
+    private StringBuffer buffer;
 
+    public void begin(String elementName) {
+        buffer = new StringBuffer();
+    }
+
+    public void text(char[] characters, int start, int length) {
+        buffer.append(characters, start, length);
+    }
+
+    public Object end(String elementName) throws TagalogParseException {
+        PersonTag parent = (PersonTag) requireAncestor("person", PersonTag.class);
+        if (elementName.equals("first-name"))
+            parent.person.setFirstName(buffer.toString());
+        else if (elementName.equals("last-name"))
+            parent.person.setLastName(buffer.toString());
+        return null;
+    }
 }
