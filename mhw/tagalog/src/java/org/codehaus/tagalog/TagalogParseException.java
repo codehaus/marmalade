98bf0c6ee464952d97d48a9a55c6efa03a35a37d
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-11 12:41:23 +0000

    Create an exception that can be thrown by Tag implementations.

---
+++ mhw/tagalog/src/java/org/codehaus/tagalog/TagalogParseException.java
@@ -0,0 +1,48 @@
+/*
+ * $Id$
+ */
+
+package org.codehaus.tagalog;
+
+/**
+ * TagalogParseException
+ *
+ * @author <a href="mailto:mhw@kremvax.net">Mark Wilkinson</a>
+ * @version $Revision$
+ */
+public class TagalogParseException extends Exception {
+
+    /**
+     * 
+     */
+    public TagalogParseException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public TagalogParseException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public TagalogParseException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public TagalogParseException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
