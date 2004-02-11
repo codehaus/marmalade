5ba57e5ea0dddae194e902491ccea133ac0aaefc
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-11 12:42:27 +0000

    Keep up-to-date with the Tag interface. Begin adding convenience methods that mimic those provided by Jelly's TagSupport class.

--- mhw/tagalog/src/java/org/codehaus/tagalog/AbstractTag.java
+++ mhw/tagalog/src/java/org/codehaus/tagalog/AbstractTag.java
@@ -4,6 +4,8 @@
 
 package org.codehaus.tagalog;
 
+import java.util.Map;
+
 /**
  * AbstractTag
  *
@@ -11,14 +13,58 @@ package org.codehaus.tagalog;
  * @version $Revision$
  */
 public abstract class AbstractTag implements Tag {
-    public void begin() {
+    private Map context;
+
+    private Tag parent;
+
+    public void setContext(Map context) {
+        this.context = context;
     }
 
-    public Object end() {
+    protected Map getContext() {
+        return context;
+    }
+
+    public void setParent(Tag parent) {
+        this.parent = parent;
+    }
+
+    public Tag getParent() {
+        return parent;
+    }
+
+    public void begin(String elementName) {
+    }
+
+    public void text(char[] characters, int start, int length) {
+    }
+
+    public void child(Object child) {
+    }
+
+    public Object end(String elementName) throws TagalogParseException {
         return null;
     }
 
     public boolean recycle() {
         return true;
+    }
+
+    //
+    // Convenience methods to make tag implementation easier.
+    //
+
+    protected Tag findAncestorWithClass(Class tagClass) {
+        return TagUtils.findAncestorWithClass(getParent(), tagClass);
+    }
+
+    protected Tag requireAncestor(String tagName, Class tagClass)
+        throws TagalogParseException
+    {
+        Tag tag = findAncestorWithClass(tagClass);
+        if (tag == null) {
+            throw new TagalogParseException(tagName + " ancestor not found");
+        }
+        return tag;
     }
 }
