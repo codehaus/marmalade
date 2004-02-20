5cf333156ec36e4e3ceb1c941ae11482ac198f62
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-20 18:36:05 +0000

    Remove Eclipse boilerplate.

--- mhw/tagalog/src/java/org/codehaus/tagalog/TagalogParseException.java
+++ mhw/tagalog/src/java/org/codehaus/tagalog/TagalogParseException.java
@@ -11,38 +11,19 @@ package org.codehaus.tagalog;
  * @version $Revision$
  */
 public class TagalogParseException extends Exception {
-
-    /**
-     * 
-     */
     public TagalogParseException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @param message
-     */
     public TagalogParseException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @param cause
-     */
     public TagalogParseException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @param message
-     * @param cause
-     */
     public TagalogParseException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
-
 }
