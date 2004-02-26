3287498e26b78f50f4c2aa6d588179081709d014
mhw <mhw@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-02-26 17:38:47 +0000

    Clarify that TagalogParseException is a complete failure in the parser.
    Add the parseErrors method to the parser.

--- mhw/tagalog/src/java/org/codehaus/tagalog/TagalogParser.java
+++ mhw/tagalog/src/java/org/codehaus/tagalog/TagalogParser.java
@@ -21,8 +21,8 @@ public interface TagalogParser {
      * @return The object returned by the {@link Tag#end()} method of the
      * root element.
      *
-     * @throws TagalogParseException if there is some problem parsing the
-     * content.
+     * @throws TagalogParseException if there was a serious problem that
+     * prevented the content from being parsed.
      */
     Object parse() throws TagalogParseException;
 
@@ -36,8 +36,19 @@ public interface TagalogParser {
      * @return The object returned by the {@link Tag#end()} method of the
      * root element.
      *
-     * @throws TagalogParseException if there is some problem parsing the
-     * content.
+     * @throws TagalogParseException if there was a serious problem that
+     * prevented the content from being parsed.
      */
     Object parse(Map context) throws TagalogParseException;
+
+    /**
+     * Return the parse errors that were collected during the parse. These
+     * are the errors that are raised by tags during the normal parsing of
+     * the content. They are tagged with the location in the source file
+     * where the error occurred.
+     *
+     * @return An array containing the parse errors, or <code>null</code>
+     * if no errors were found.
+     */
+    ParseError[] parseErrors();
 }
