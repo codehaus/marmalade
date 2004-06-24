26a9b96c6e51788c0b4fbc5af20d4d495f5e8d34
jdcasey <jdcasey@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-06-24 04:44:46 +0000

    added for convenient support of reflective access to classes/objects. This is a CommonJava donation, and is mature code, even though we don't have unit tests for it as yet.

---
+++ marmalade/marmalade-utils/src/test/java/org/codehaus/marmalade/util/ReflectorTest.java
@@ -0,0 +1,15 @@
+/* Created on Jun 24, 2004 */
+package org.codehaus.marmalade.util;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jdcasey
+ */
+public class ReflectorTest extends TestCase {
+    
+    public void testEmpty() {
+        
+    }
+
+}
