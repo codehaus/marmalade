09268093b0eae8e7814111e52540b91ce6296412
jdcasey <jdcasey@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-06-24 01:40:50 +0000

    fixed minor bug with embedded import script xmlns.

--- marmalade/marmalade-taglibs/jelly-core/src/test/java/org/codehaus/marmalade/tags/jelly/core/ImportTagTest.java
+++ marmalade/marmalade-taglibs/jelly-core/src/test/java/org/codehaus/marmalade/tags/jelly/core/ImportTagTest.java
@@ -14,22 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Copyright 2001-2004 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 /* Created on Apr 15, 2004 */
 package org.codehaus.marmalade.tags.jelly.core;
 
@@ -58,7 +42,7 @@ import java.util.TreeMap;
 public class ImportTagTest extends TestCase
 {
     private static final String SCRIPT = "<?xml version=\"1.0\"?>\n\n"
-        + "<set xmlns=\"marmalade:jstl-core\" var=\"testKey\" value=\"testValue\"/>\n\n";
+        + "<set xmlns=\"marmalade:jelly-core\" var=\"testKey\" value=\"testValue\"/>\n\n";
     private File scriptFile;
 
     public void testDoExecute_StringUrl_ParseOnly(  )
