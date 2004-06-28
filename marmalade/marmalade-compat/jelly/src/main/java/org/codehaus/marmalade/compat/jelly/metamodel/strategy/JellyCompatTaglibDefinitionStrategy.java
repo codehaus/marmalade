e0b5f5067859cf5503c0ff58c23c967160ce3c9e
jdcasey <jdcasey@58ec8b93-5d01-0410-b32d-d264b9d2dd6f>
2004-06-28 20:25:22 +0000

    first crack at a jelly compatibility layer for marmalade...NOT TESTED.

---
+++ marmalade/marmalade-compat/jelly/src/main/java/org/codehaus/marmalade/compat/jelly/metamodel/strategy/JellyCompatTaglibDefinitionStrategy.java
@@ -0,0 +1,38 @@
+/* Created on Jun 28, 2004 */
+package org.codehaus.marmalade.compat.jelly.metamodel.strategy;
+
+import org.codehaus.marmalade.compat.jelly.model.MarmaladeCompatibleJellyTaglib;
+import org.codehaus.marmalade.metamodel.strategy.TaglibDefinitionStrategy;
+import org.codehaus.marmalade.model.MarmaladeTagLibrary;
+
+/**
+ * @author jdcasey
+ */
+public class JellyCompatTaglibDefinitionStrategy
+    implements TaglibDefinitionStrategy
+{
+    public static final String JELLY_TAGLIB_PREFIX = "jelly";
+    private MarmaladeCompatibleJellyTaglib marmaladeTaglib;
+
+    public JellyCompatTaglibDefinitionStrategy(  )
+    {
+        this.marmaladeTaglib = new MarmaladeCompatibleJellyTaglib(  );
+    }
+
+    public MarmaladeTagLibrary resolve( String prefix, String taglib )
+    {
+        MarmaladeTagLibrary tlib = null;
+
+        // Greedily snap up anything with a jelly: uri prefix.
+        //
+        // If you want to use marmalade-native jelly taglibs, use the 
+        // new "marmalade:jelly-core" uri format
+        //
+        if ( JELLY_TAGLIB_PREFIX.equals( prefix ) )
+        {
+            tlib = marmaladeTaglib;
+        }
+
+        return tlib;
+    }
+}
